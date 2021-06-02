bc0d39cb1711bab3b55a77e171482f0f8d4d28a9
Ubuntu <ubuntu@ip-172-31-93-33.ec2.internal>
2021-06-02 18:08:05 +0000

    this progrma calcualtes the area of a triangle using functions to minimize the strain on the computer

---
+++ area_of_triangles_with_function.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Created by: Liam Csiffary
+# Created on: June 2, 2021
+# This program calculates the area of a triangle
+# with given inputs
+
+
+# function that does gets the parameters and then calcualtes
+# the area, then prints it to the user
+def calc_area(length, height):
+    area = length * height / 2
+    print("The area is {:.2f}".format(area))
+
+
+def main():
+    print("This program calcualtes the area of a triangle")
+    # get dimentions
+    user_length = input("What is the length of your triangle: ")
+    user_height = input("What is the height of your triangle: ")
+
+    # make sure the users nums can be integers
+    try:
+        user_length = float(user_length)
+        user_height = float(user_height)
+
+        if (user_height > 0 and user_length > 0):
+            # can the function and pass it the parameters
+            calc_area(user_length, user_height)
+        else:
+            print("Numbers must be positive")
+
+    # if there is an error converting the users input
+    except ValueError:
+        print("Not valid input")
+
+
+if __name__ == "__main__":
+    main()
